f874bc126b3d36b7aa2e80d5c9d53d9262d6908b
steve_han <809170493@qq.com>
2020-04-01 12:23:02 +0800

    第二次提交

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -3,6 +3,7 @@ package com.example.demo3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Test
 @SpringBootApplication
 public class Demo3Application {
 
